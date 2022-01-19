a447332c20b0eafaeeb7cff662f62f65065404f7
pterolycus9pn <pngwira7@gmail.com>
2022-01-19 11:24:42 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ab027bacac4f202886da358447efeeab'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd91eb1d1-009d-4ae0-963e-41bf1b5fbc77'
+};
