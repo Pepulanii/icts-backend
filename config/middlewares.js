9bbdc149d3a12a4876b39ca60e3a90e94dbbaa3a
Pepulani <pngwira7@gmail.com>
2023-02-27 12:08:48 +0200

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,14 +1,35 @@
-module.exports = {
-  //...
-  settings: {
-    parser: {
-      enabled: true,
-      multipart: true,
-      formidable: {
-        maxFileSize: 200 * 1024 * 1024 // Defaults to 200mb
-      }
-    }
-  },
-  //...
-};
+module.exports = ({ env }) => ({
+  // ...
 
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  settings: {
+    cors: {
+      enabled: true,
+      headers: "*",
+      origin: ["http://localhost", "https://foo.example"],
+    },
+  },
+  name: "strapi::body",
+  config: {
+    formLimit: "256mb", // modify form body
+    jsonLimit: "256mb", // modify JSON body
+    textLimit: "256mb", // modify text body
+    formidable: {
+      maxFileSize: 200 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+    },
+  },
+  // ...
+});
