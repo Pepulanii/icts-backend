de0fb7a8d01f9659767f1b76bc2e619d41b86c18
Pepulani <pngwira7@gmail.com>
2023-03-10 08:15:47 +0200

    upload config via plugin.js

---
+++ config/plugins.js
@@ -0,0 +1,27 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      providerOptions: {
+        localServer: {
+          maxage: 300000,
+        },
+      },
+      sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64,
+      },
+    },
+  },
+
+  "users-permissions": {
+    config: {
+      jwt: {
+        expiresIn: "7d",
+      },
+    },
+  },
+});
