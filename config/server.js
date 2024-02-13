a2ba9fdac3b13184c68421e662670019aaedad05
lusentomba <100279708+lusentomba@users.noreply.github.com>
2024-02-13 07:45:43 +0200

    Projects models added

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  url: env("", "http://localhost:1337"),
+  host: env("APP_HOST", "0.0.0.0"),
+  port: env.int("NODE_PORT", 1337),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "ab027bacac4f202886da358447efeeab"),
